00565994169819c2c97c59f89e2bf799ee28c246
mar <tigra_84@mail.ru>
2019-09-29 02:30:11 +0300

    fixed artefacts after key pressing

--- src/run_rl.cpp
+++ src/run_rl.cpp
@@ -12,7 +12,7 @@
 
 int main() {
 //	std::cout << Board::widthBoard << " " << Board::heightBoard << std::endl;
-	std::unique_ptr<Figure> owner;
+	std::shared_ptr<Figure> owner;
 	int ch;
 	std::vector<std::vector<uint8_t>> points;
 	Board& board = Board::instance();
@@ -24,7 +24,6 @@ int main() {
 	
 	int x = painter.getScreenWidth();
 	int y = painter.getScreenHeight();
-	painter.drawPoint(0, 0, '#', 6, 0);
 	uint32_t originX = (x - Board::widthBoard) / 2;
 	uint32_t originY = 0.1 * y;
 	painter.drawBoard(board, originX,originY);
@@ -33,13 +32,11 @@ int main() {
 	painter.drawFigure(*owner, originX, originY);
 	while (true) {
 
-		if(kbhit()) {
-			ch = rlutil::getkey(); // Get character
+		if(ch = getch()) {
 			if (ch == 'q')
 				break;
 			switch (ch) {
 				case 'w':
-				case rlutil::KEY_UP:
 				case rlutil::KEY_SPACE:
 					if (board.allowRotate(*owner)) {
 						painter.drawFigure(*owner, originX, originY, false);
@@ -47,21 +44,18 @@ int main() {
 					}
 					break;
 				case 'a':
-				case rlutil::KEY_LEFT:
 					if (board.allowMove(Direction::Left, *owner)) {
 						painter.drawFigure(*owner, originX, originY, false);
 						owner->move(Direction::Left);
 					}
 					break;
 				case 'd':
-				case rlutil::KEY_RIGHT:
 					if (board.allowMove(Direction::Right, *owner)) {
 						painter.drawFigure(*owner, originX, originY, false);
 						owner->move(Direction::Right);
 					}
 					break;
 				case 's':
-				case rlutil::KEY_DOWN:
 					if (board.allowMove(Direction::Down, *owner)) {
 						painter.drawFigure(*owner, originX, originY, false);
 						owner->move(Direction::Down);
@@ -70,10 +64,10 @@ int main() {
 				default:
 					break;
 			}
+			
 			ch = 0;
 		}
-		if (ch == 'q')
-			break;
+		
 		auto end = std::chrono::system_clock::now();
 		std::chrono::duration<double> diff = end-start;
 		if (diff.count() > 1.5) {
@@ -88,7 +82,6 @@ int main() {
 		painter.drawFigure(*owner, originX, originY, false);
 		painter.drawFigure(*owner, originX, originY);
 //		std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
 	}
 	rlutil::showcursor();
 	
